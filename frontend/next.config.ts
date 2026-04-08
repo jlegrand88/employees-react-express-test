1b5f698580c4a45dc61814588260369b305ca15a
jlegrand88 <javier.legrand@gmail.com>
2026-04-07 23:20:02 -0400

    TEST GH pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,16 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/employees-react-express-test",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/api/portraits/**',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/api/portraits/**",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
